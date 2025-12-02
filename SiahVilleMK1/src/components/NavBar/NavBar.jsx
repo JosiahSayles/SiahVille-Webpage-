f128cb45fa74c4c6ddc36d57f80e056d7567e7bc
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-12-01 23:39:10 -0700

    changed background could and NavIcons styling

--- SiahVilleMK1/src/components/NavBar/NavBar.jsx
+++ SiahVilleMK1/src/components/NavBar/NavBar.jsx
@@ -3,39 +3,50 @@ import { Link, NavLink } from "react-router";
 
 export default function NavBar() {
   return (
-    <header className="fixed top-0 left-0 w-full  shadow-md z-50 flex justify-evenly  items-baseline bg-black shadow-md px-6 py-3">
+    <header className="fixed top-0 left-0 w-full  shadow-xl z-50 flex justify-evenly  items-baseline bg-slate-500 shadow-md px-6 py-3">
       <div className="flex ">
-        <NavLink className="flex-col items text-white pr-5">
-          <img src="/Icons/home.png" alt="Home Icon" />
+        <NavLink className="flex-col items text-white pr-5 hover:text-red-400">
+          <img className="pl-2 " src="/Icons/home.png" alt="Home Icon" />
           Home
         </NavLink>
-        <NavLink className="flex-col items text-white pr-5">
-          <img src="/Icons/shoppingbag.png" alt="Shopping bag Icon" />
+        <NavLink className="flex-col items text-white pr-5 hover:text-red-400">
+          <img
+            className="pl-8"
+            src="/Icons/shoppingbag.png"
+            alt="Shopping bag Icon"
+          />
           Shopping Bag
         </NavLink>
-        <NavLink className="flex-col items text-white pr-5">
-          <img src="/Icons/login.png" alt="Login Icon" />
+        <NavLink className="flex-col items text-white  pr-5 hover:text-red-400">
+          <img className="pl-1" src="/Icons/login.png" alt="Login Icon" />
           Login
         </NavLink>
       </div>
-      <Link to="/" className="flex items-center justify-center">
+      <Link
+        to="/"
+        className="flex items-center justify-center shadow-lg  rounded-3xl"
+      >
         <img
           src="/img/SiahVilleLogo.png"
           alt="SiahVilleLogo"
-          className="w-40 h-40 object contain"
+          className="w-40 h-30 object contain"
         />
       </Link>
       <div className="flex ">
-        <NavLink className="flex-col items text-white pr-5">
-          <img src="/Icons/search.png" alt="Search Icon" />
+        <NavLink className="flex-col items text-white pr-5 hover:text-red-400 ">
+          <img className="pl-3" src="/Icons/search.png" alt="Search Icon" />
           Search
         </NavLink>
-        <NavLink className="flex-col items text-white pr-5">
-          <img src="/Icons/collections.png" alt="Shopping bag Icon" />
+        <NavLink className="flex-col items text-white pr-5 hover:text-red-400">
+          <img
+            className="pl-7"
+            src="/Icons/collections.png"
+            alt="Shopping bag Icon"
+          />
           Collections
         </NavLink>
-        <NavLink className="flex-col items text-white pr">
-          <img src="/Icons/catalog.png" alt="Login Icon" />
+        <NavLink className="flex-col items text-white pr hover:text-red-400">
+          <img className="pl-4" src="/Icons/catalog.png" alt="Login Icon" />
           Catalog
         </NavLink>
       </div>
